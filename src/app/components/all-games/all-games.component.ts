c312cd7b30e272e958b2a7085ba18211e71e31aa
Oleksandr Rybalka <Oleksandr_Rybalka@epam.com>
2022-02-20 17:57:45 +0200

    commit

--- src/app/components/all-games/all-games.component.ts
+++ src/app/components/all-games/all-games.component.ts
@@ -6,22 +6,23 @@ import { Game } from 'app/game';
 @Component({
   selector: 'app-all-games',
   templateUrl: './all-games.component.html',
-  styleUrls: ['./all-games.component.less']
+  styleUrls: ['./all-games.component.less'],
 })
 export class AllGamesComponent implements OnInit {
-  gamesCtrl=function ($scope:any) {
+  /* gamesCtrl = function ($scope: any) {
     $scope.games = GAMES;
-  }
+  }; */
   //games = GAMES;
   games: Game[] = [];
-  searchStr='';
-  constructor(private gameService: GameService) { }
-  
+  searchStr = '';
+  constructor(private gameService: GameService) {}
+
   ngOnInit(): void {
     this.getGames();
   }
   getGames(): void {
-    this.gameService.getGames()
-        .subscribe(games => this.games = games);
+    this.gameService
+      .getGames()
+      .subscribe((games: Game[]) => (this.games = games));
   }
 }
--- src/app/components/game-search/game-search.component.ts
+++ src/app/components/game-search/game-search.component.ts
@@ -2,38 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 import { Observable, Subject } from 'rxjs';
 
-import {
-   debounceTime, distinctUntilChanged, switchMap
- } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 import { Game } from 'app/game';
 import { GameService } from 'app/game.service';
+import { SearchPipe } from 'app/search.pipe';
 
 @Component({
   selector: 'app-game-search',
   templateUrl: './game-search.component.html',
-  styleUrls: ['./game-search.component.less']
+  styleUrls: ['./game-search.component.less'],
 })
 export class GameSearchComponent implements OnInit {
-  games$!: Observable<Game[]>;
-  private searchTerms = new Subject<string>();
-  constructor(private gameService: GameService) { }
-
-  search(term: string): void {
-    this.searchTerms.next(term);
+  searchStr = '';
+  search(value: string) {
+    return value;
   }
-  searchStr='';
-  ngOnInit(): void {
-    /* this.games$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
-
-      // ignore new term if same as previous term
-      distinctUntilChanged(),
-
-      // switch to new search observable each time the term changes
-      switchMap((term: string) => this.gameService.searchGames(term)),
-    ); */
-  }
-
+  ngOnInit(): void {}
 }
